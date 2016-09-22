4c41a16c27398ead0db9f4f657f7c1664825c81f
Jacek Zietek <jacek.zietek@gft.com>
2016-09-22 10:36:47 +0200

    Added edges handling

--- scripts/controls.js
+++ scripts/controls.js
@@ -6,22 +6,32 @@ var keys = {
 };
 
 var rocketState = { 
-    x : 300, 
-    y: 300, 
-    ax : 0,
-    ay : 0,
+    x : 100, 
+    y: 100, 
     vx : 0,
     vy : 0,
     angle: 0,
-    thrust : 0
+    thrust : 0,
+    width : 0,
+    height : 0
 };
 
 var planetState = {
-    x : 500,
-    y : 500
-}
+    x : 0,
+    y : 0,
+    g : 1,
+    width : 0,
+    height : 0
+};
 
-$(document).ready(function () {
+var spaceState = {
+    width : 0,
+    height : 0
+};
+
+$(document).ready(function () {    
+    initAll();
+    
     document.addEventListener('keydown', function(event) {
         if(event.keyCode == 40) {
             keys.downKeyPressed = true;
@@ -55,6 +65,22 @@ $(document).ready(function () {
     function calculateRocketPositionOffset(rState, pixelShiftMultiplier) {
         rState.y = rState.y + rState.vy * pixelShiftMultiplier;
         rState.x = rState.x + rState.vx * pixelShiftMultiplier;
+
+        //Edge check
+        var margin = 160;
+        if (rState.y < -1 * rState.height - margin) {
+            rState.y = spaceState.height + margin;
+        }
+        else if (rState.y > spaceState.height + margin) {
+            rState.y = -1 * rState.height - margin;
+        }
+
+        if (rState.x < -1 * rState.width - margin) {
+            rState.x = spaceState.width + margin;
+        }
+        else if (rState.x > spaceState.width + margin) {
+            rState.x = -1 * rState.width - margin;
+        }
     }
 
     function setThrustAndAngle(rState)
@@ -82,22 +108,35 @@ $(document).ready(function () {
         rState.vy = rState.vy + (0.01 * rState.thrust * Math.sin((rState.angle + 225) * (Math.PI/180)));      
     }
 
+    function initAll() {
+        //Space part
+        spaceState.width = window.innerWidth - 30;
+        spaceState.height = window.innerHeight - 30;               
+        $("#space").css("height", spaceState.height);
+        $("#space").css("width", spaceState.width);
+
+        //Planet part        
+        var p = document.getElementById("planet");
+        planetState.width = p.width;
+        planetState.height = p.height;
+        planetState.x = (spaceState.width / 2) - (planetState.width/2);
+        planetState.y = (spaceState.height / 2) - (planetState.height/2);
+
+        p.style.top = planetState.y + "px";
+        p.style.left = planetState.x + "px";
+    }
+
     //Calculate every 20 ms
     setInterval(function () {          
         setThrustAndAngle(rocketState);
-       
+               
         calculateRocketVelocities(rocketState);
  
         calculateRocketPositionOffset(rocketState, 2);        
     }, 20);
 
     //Animate at 25 FPS (every 40 ms)
-    setInterval(function () {
-        //Planet part
-        var p = document.getElementById("planet");
-        p.style.top = planetState.y + "px";
-        p.style.left = planetState.x + "px";
-
+    setInterval(function () {       
         //Rocket part
         var r = document.getElementById("rocket");
         r.style.top = rocketState.y + "px";
